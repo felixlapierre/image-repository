7a06983b78dc9be8cc9b2d916e18a663931f5ff9
Felix <felix.lapierre1@gmail.com>
2020-02-11 11:24:46 -0500

    Added authentication with passing tests

---
+++ test/authRequest.js
@@ -0,0 +1,28 @@
+const crypto = require('crypto');
+const request = require('supertest');
+const users = {
+    mike: {
+        key: "a5b9731b-240a-4d9d-bbf5-8bd35502e16e",
+        secret: "mike-secret"
+    },
+    frankie: {
+        key: "67f66d5e-19e8-4162-bf7f-6f1be73d6537",
+        secret: "frankie-secret"
+    },
+    rebecca: {
+        key: "1c383b31-3c44-413c-a286-5d07562a582d",
+        secret: "rebecca-secret"
+    }
+}
+
+function getAuthorizationHeader(key, secret, date) {
+    const signature = `date: ${date}`;
+    const encoded = crypto.createHmac('sha256', secret).update(signature).digest('base64');
+    return `Signature keyId="${key}",algorithm="hmac-sha256",signature="${encoded}"`;
+}
+
+module.exports = function(user) {
+    const date = new Date().toGMTString();
+    const authorizationHeader = getAuthorizationHeader(users[user].key, users[user].secret, date);
+    return {auth: authorizationHeader, date: date}
+}
--- test/test.js
+++ test/test.js
@@ -3,27 +3,38 @@ const expect = require('chai').expect;
 let app = require('../src/index');
 let database = require('../src/database');
 const sampleImages = require('./sampleImages');
+const getAuthHeaders = require('./authRequest');
+let auth, date;
 
 describe('app', () => {
     before(() => {
         database.clearImages();
     })
+    beforeEach(() => {
+        ({auth, date} = getAuthHeaders('mike'));
+    })
     describe('Status', () => {
         it('should return status 200 on GET /', async () => {
-            return request(app)
+            return request(app, 'mike')
                 .get('/')
+                .set('Authorization', auth)
+                .set('Date', date)
                 .expect(200)
         })
     })
     describe('Uploading images', () => {
         it('should support uploading single images', async () => {
-            return request(app)
+            return request(app, 'mike')
                 .post('/image')
+                .set('Authorization', auth)
+                .set('Date', date)
                 .send({ image: sampleImages.sample })
                 .expect(200)
                 .then((response) => {
-                    return request(app)
+                    return request(app, 'mike')
                         .get(`/image/${response.body.uuid}`)
+                        .set('Authorization', auth)
+                        .set('Date', date)
                         .expect(200)
                 }).then((response) => {
                     expect(response.body.base64).to.equal(sampleImages.sample.base64);
@@ -33,17 +44,23 @@ describe('app', () => {
         it('should support uploading several images', async () => {
             const images = [sampleImages.blue, sampleImages.matterhorn];
 
-            return request(app)
+            return request(app, 'mike')
                 .post('/image/bulk')
+                .set('Authorization', auth)
+                .set('Date', date)
                 .send({ images: images})
                 .expect(200)
                 .then((response) => {
                     return Promise.all([
-                        request(app)
+                        request(app, 'mike')
                         .get(`/image/${response.body.images[0].uuid}`)
+                        .set('Authorization', auth)
+                        .set('Date', date)
                         .expect(200),
-                        request(app)
+                        request(app, 'mike')
                         .get(`/image/${response.body.images[1].uuid}`)
+                        .set('Authorization', auth)
+                        .set('Date', date)
                         .expect(200)
                     ])
                 }).then((responses) => {

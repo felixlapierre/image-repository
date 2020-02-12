c5f65a5f5423262b7824145f6796b77ceb96fc37
Felix <felix.lapierre1@gmail.com>
2020-02-12 16:46:32 -0500

    Added search bar to frontend

--- frontend/app.ts
+++ frontend/app.ts
@@ -1,28 +1,38 @@
 import express = require('express')
 import path = require('path');
-import {Request, RequestOptions} from './request';
+import { Request, RequestOptions } from './request';
 
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 
-app.get("/", (req, res) => {
-    Request({path: '/image/all', method: 'get'}, 'mike').then((response) => {
-        if(response.data.images.length == 0) {
-            res.render("homepage", {images: ["No images to be displayed"]});
-            return;
-        }
-        Promise.all(response.data.images.map((uuid) => {
-            return Request({path: `/image/${uuid}`, method: 'GET'}, 'mike')
+function renderSearchResults(images, res) {
+    if (images.length == 0) {
+        res.render("header", {});
+    } else {
+        Promise.all(images.map((uuid) => {
+            return Request({ path: `/image/${uuid}`, method: 'GET' }, 'mike')
         })).then((responses) => {
             const images = responses.map((response: any) => {
                 return response.data;
             })
 
-            res.render("homepage", {images: images});
+            res.render("content", { images: images });
         })
+    }
+}
+
+app.get("/", (req, res) => {
+    Request({ path: '/image/all', method: 'get' }, 'mike').then((response) => {
+        renderSearchResults(response.data.images, res);
     });
+})
+
+app.get("/search", (req, res) => {
+    Request({ path: `/search?query=${req.query.search}`, method: 'get' }, 'mike').then((response) => {
+        renderSearchResults(response.data.images, res);
+    })
 })
 
 const port = 4000;
